b4bb7d50ba7598b06ac400ae2b9d2c952c643e7a
SerasaLimpaNome <162386452+SerasaLimpaNome@users.noreply.github.com>
2024-03-06 19:16:23 -0300

    Add files via upload

---
+++ sla.js
@@ -0,0 +1,13 @@
+const greeting = '   Hello world!   ';
+
+console.log(greeting);
+// Expected output: "   Hello world!   ";
+
+console.log(greeting.trim());
+// Expected output: "Hello world!";
+
+if (!String.prototype.trim) {
+  String.prototype.trim = function() {
+    return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, "");
+  };
+}
